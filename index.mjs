fa317748c83aa6a25d9038648cb481350c1b2584
chchch <charles.li@cnrs.fr>
2024-10-31 10:08:17 +0100

    bugfix

--- index.mjs
+++ index.mjs
@@ -67,8 +67,9 @@ const alignCheck = async () => {
 
     const ret = await alignWordsplits(text,tam,eng,[],lookup);
     
-    makeAlignmentTable(ret.alignment,tamlines.map(l => l.replaceAll(/\/.+?(?=\s|$)/g,'')),ret.warnings);
-    
+    const tables = makeAlignmentTable(ret.alignment,tamlines.map(l => l.replaceAll(/\/.+?(?=\s|$)/g,'')),ret.warnings);
+    warnings.append(...tables); 
+
     if(lookup) inputs[2].value = refreshTranslation(tamlines,ret.wordlist);
 
     const parser = new DOMParser();
